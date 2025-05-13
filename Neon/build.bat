8bad7d60df5f87ee2336a9b4f3fff79f14a2be70
Cycarillic <tmhatre1251@gmail.com>
2025-05-13 21:36:08 +0530

    Add files via upload

---
+++ Neon/build.bat
@@ -0,0 +1,4 @@
+cd "build"
+g++ Neon.cpp -o Neon
+
+Neon
---
+++ Neon/build/Neon.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+
+//Configure the Parser
+class config{
+    public:
+        //NOTE - Using This will significantly reduce performance
+        bool show_parsed_files = false;
+
+        //Saved file name
+        std::string outfilename = "out.scene";
+
+        //Save the Output Recommended
+        bool save_file = true ;
+};
+
+
+
+//Token class !!
+class Tokens{
+
+    public:
+        std::string objname;
+        std::string identifier;
+        int posx;
+        int posy;
+        int posz;
+        int rotation;
+        int scale;
+
+
+};
+
+class tokenizer{
+    public:
+        void tokenize(std::string line,Tokens& tokens){
+            tokens = Tokens();
+            std::istringstream iss(line);
+            std::string curr_char;
+            int count = 0;
+            while (iss >> curr_char){
+                if (count == 0){
+                    tokens.objname = curr_char;
+                } else if (count == 1){
+                    tokens.identifier = curr_char;
+                }else{
+                    //Check if the data is not 1 char long
+                    if (curr_char.length() < 2) continue;
+                    char type = curr_char[0];
+                    int dat = std::stoi(curr_char.substr(1));
+                    //Very bad ik but it works
+                    if (type == 'x'){
+                        tokens.posx = dat;
+                    }else if(type == 'y'){
+                        tokens.posy = dat;
+                    }else if (type == 'z'){
+                        tokens.posz = dat;
+                    }else if (type == 'r'){
+                        tokens.rotation = dat;
+                    }else if (type == 's'){
+                        tokens.scale = dat;
+                    }else {
+                        std::cout << "Invalid data type: " << curr_char ; 
+                        break;
+                    }
+
+                }
+                ++count;
+            }
+            
+        }
+
+};
+
+int main(){
+    //Init the classes
+    Tokens tokens;
+    tokenizer lexer;
+    config Config;
+
+    //Useless var
+    std::string uselesssvar;
+
+    //File stuff
+    std::ifstream file;
+    std::ofstream outfile(Config.outfilename);
+    std::string filename;
+    std::cout << "Enter FileName: ";
+    std::cin >> filename;
+
+    file.open(filename,std::ios::in);
+    
+    //Variables
+    std::string line;
+    int parsed_objs = 0;
+
+    
+    //Reading the file stuff
+    int counter = 0;
+    std::string temp;
+
+    //Start timer
+    auto start = std::chrono::high_resolution_clock::now();
+
+    while (std::getline(file,line)){
+        line.erase(0, line.find_first_not_of(" \t\r\n"));
+
+        // Skip empty lines or comments
+        if (line.empty() or line[0] == '#' or (line.size() >= 2 && line[0] == '/' && line[1] == '/')) {
+        continue;
+        }
+        lexer.tokenize(line,tokens);
+        /*
+        std::cout << "name : " << tokens.objname << "\n"
+        << "id : " << tokens.identifier << "\n"
+        << "x : " << tokens.posx << "\n"
+        <<  "y : " << tokens.posy << "\n"
+        << "z : " << tokens.posz << "\n"
+        << "r : " << tokens.rotation << "\n"
+        << "s : " << tokens.scale << "\n";
+        */
+        
+        /*
+        Object{ # SceneObject
+    TemplateName : 'platformwood0',
+    Identifier : 'obj21',
+    Position{ # Vector2
+        X : 2232.24462890625,
+        Y : -849.1557006835938,
+    }
+    Depth : 0.0,
+    Rotation : 0.0,
+    Scaling : 1.0,
+    LocalAabb{ # Rectangle
+        X : -54.653690338134766,
+        Y : -6.8772125244140625,
+        Width : 118.49516296386719,
+        Height : 53.251014709472656,
+    }
+    Hidden : 0,
+}
+    */
+        if (Config.show_parsed_files == true){
+            std::cout << "Object{" << "\n"
+            << "\t" << "TemplateName : " << tokens.objname << "\n"
+            << "\t" << "Identifier : " << tokens.identifier << "\n"
+            << "\t" << "Position{" << "\n"
+            << "\t\t" << "X : " << tokens.posx << "\n"
+            << "\t\t" << "Y : " << tokens.posy << "\n"
+            << "\t" << "}" << "\n"
+            << "\t" << "Depth : " << tokens.posz << "\n"
+            << "\t" << "Rotation : " << tokens.rotation << "\n"
+            << "\t" << "Scaling : " << tokens.scale << "\n"
+            << "\t" << "Hidden : 0" << "\n"
+            << "}" << "\n";
+        }
+        //Saving file
+        if (Config.save_file == true){
+            outfile << "Object{" << "\n"
+                << "\t" << "TemplateName : " << tokens.objname << "\n"
+                << "\t" << "Identifier : " << tokens.identifier << "\n"
+                << "\t" << "Position{" << "\n"
+                << "\t\t" << "X : " << tokens.posx << "\n"
+                << "\t\t" << "Y : " << tokens.posy << "\n"
+                << "\t" << "}" << "\n"
+                << "\t" << "Depth : " << tokens.posz << "\n"
+                << "\t" << "Rotation : " << tokens.rotation << "\n"
+                << "\t" << "Scaling : " << tokens.scale << "\n"
+                << "\t" << "Hidden : 0" << "\n"
+                << "}" << "\n";
+        }
+        ++parsed_objs;
+    }  
+
+    
+    //show the objs
+    std::cout << "Parsed : " <<parsed_objs << " Objects" << std::endl;
+    // End timer
+    auto end = std::chrono::high_resolution_clock::now();  
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "Parsing took " << duration.count() << " ms.\n";
+
+    if (Config.show_parsed_files == true){
+        //Prevent exit :l
+        std::cout << "press enter to end the program";
+        std::cin >> uselesssvar;
+        }
+    
+    return 0;
+}
